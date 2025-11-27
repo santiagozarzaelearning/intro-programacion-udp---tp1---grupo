654b406661c60c662b0f85d3008a70d4e2396e66
SantiagoZarzaElearning <santiago_zarza_Elarning@outlook.com>
2025-11-27 20:23:27 -0300

    First commit
    Initial commit made by Ornela

---
+++ TP1IP.py
@@ -0,0 +1,30 @@
+cantHuespedesTotal = 0
+ciudadMasHabitaciones = ""
+cantHabitacionesMas = 0
+for i in range (10 - 1):
+    nombreCiudadFilial = str(input("Ingrese el nombre de la ciudad de la filial: "))
+    capacidadTotalFilial = int(input("Ingrese la capacidad total de la filial: "))
+    cantHabitaciones = int(input("Ingrese la cantidad de habitaciones: "))
+    cantHuespedesMes = int(input("Ingrese la cantidad de huespedes por mes: "))
+    # Validaciones 
+    if capacidadTotalFilial <= 0 or cantHabitaciones <= 0 or cantHuespedesMes <= 0:
+        print("Error: Los valores ingresados no pueden ser menores o iguales a cero")
+        continue
+    # N1 La cantidad total de huéspedes que puede alojar toda la cadena de hoteles.
+    cantHuespedesTotal += capacidadTotalFilial
+    # N2 El porcentaje de ocupación de cada hotel.
+    cantHuespedesDiarios = cantHuespedesMes / 30  # Promedio de huespedes por día
+    porcentajeOcupacion = int((cantHuespedesDiarios/capacidadTotalFilial) * 100)
+    # N3 El nombre de la ciudad con mayor cantidad de habitaciones.
+    if cantHabitaciones >= cantHabitacionesMas:
+        if cantHabitaciones > cantHabitacionesMas:
+            cantHabitacionesMas = cantHabitaciones
+            ciudadMasHabitaciones = nombreCiudadFilial
+        else:
+            if cantHabitaciones == cantHabitacionesMas:
+                ciudadMasHabitaciones = ciudadMasHabitaciones + ", " + nombreCiudadFilial
+
+    
+    print("El porcentaje de ocupacion diario del hotel de la ciudad de", nombreCiudadFilial, "es:", porcentajeOcupacion, "%")
+print("La cantidad total de huespedes que puede tener toda la cadena de hoteles es: ", cantHuespedesTotal)
+print("La ciudad con mayor cantidad de habitaciones es: ", ciudadMasHabitaciones)
