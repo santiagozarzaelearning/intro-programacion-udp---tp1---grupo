cc1d5f9ee87ccc71fe695b19bd801ad593137e57
SantiagoZarzaElearning <santiago_zarza_Elarning@outlook.com>
2025-11-27 21:30:58 -0300

    set 'porcentajeOcupacion' with '0' to fix error on invalid input

--- TP1IP.py
+++ TP1IP.py
@@ -1,15 +1,18 @@
 cantHuespedesTotal = 0
 ciudadMasHabitaciones = ""
 cantHabitacionesMas = 0
+porcentajeOcupacion = 0
 for i in range (10 - 1):
     nombreCiudadFilial = str(input("Ingrese el nombre de la ciudad de la filial: "))
-    capacidadTotalFilial = int(input("Ingrese la capacidad total de la filial: "))
+    capacidadTotalFilial = int(input("Ingrese la capacidad maxima de huespedes de la filial: "))
     cantHabitaciones = int(input("Ingrese la cantidad de habitaciones: "))
     cantHuespedesMes = int(input("Ingrese la cantidad de huespedes por mes: "))
+    
     # Validaciones 
     if capacidadTotalFilial <= 0 or cantHabitaciones <= 0 or cantHuespedesMes <= 0:
         print("Error: Los valores ingresados no pueden ser menores o iguales a cero")
         continue
+
     # N1 La cantidad total de huéspedes que puede alojar toda la cadena de hoteles.
     cantHuespedesTotal += capacidadTotalFilial
     # N2 El porcentaje de ocupación de cada hotel.
@@ -24,7 +27,10 @@ for i in range (10 - 1):
             if cantHabitaciones == cantHabitacionesMas:
                 ciudadMasHabitaciones = ciudadMasHabitaciones + ", " + nombreCiudadFilial
 
-    
-    print("El porcentaje de ocupacion diario del hotel de la ciudad de", nombreCiudadFilial, "es:", porcentajeOcupacion, "%")
+
+#N1 output
 print("La cantidad total de huespedes que puede tener toda la cadena de hoteles es: ", cantHuespedesTotal)
+#N2 output
+print("El porcentaje de ocupacion diario del hotel de la ciudad de", nombreCiudadFilial, "es:", porcentajeOcupacion, "%")
+#N3 output
 print("La ciudad con mayor cantidad de habitaciones es: ", ciudadMasHabitaciones)
